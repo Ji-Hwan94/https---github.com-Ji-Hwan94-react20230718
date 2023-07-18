ed62e1c6b51ecaacea8b383b881158d09edd8b4f
jiHwan <ghkdrbqhd2@naver.com>
2023-07-18 10:39:23 +0900

    map

--- src/AppBackUp2.js
+++ src/AppBackUp2.js
@@ -30,6 +30,7 @@ function App() {
           {
             // map은 배열에 있는 item을 내가 원하는 무엇이듯이 바꿔주는 역할을 하고, '새로운 배열'로 return 해준다
             // 함수의 첫번째 argument(함수에 파라미터로 들어가는 것)로 현재의 item을 가져 올 수 있다는 것이다
+            // key를 설정 해준다.
             toDos.map((item, index) => (
               <li key={index}>{item}</li>
             ))
